174a68df6b12ac14d35fea4867f22c06303bf495
Connor Koehler <cfkoehler.work@gmail.com>
2022-06-13 13:16:29 +0000

    Testing endpoint

--- src/main/java/emissary/core/JVMHealthCheck.java
+++ src/main/java/emissary/core/JVMHealthCheck.java
@@ -1,14 +1,21 @@
 package emissary.core;
 
+import java.util.Map;
+
 import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.health.HealthCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // Health Check that returns healthy if JVM heap and garbage collect are at acceptable values.
 public class JVMHealthCheck extends HealthCheck {
 
     protected double maxHeapUsage = 100.00;
     protected double maxNonHeapUsage = 100.00;
+
+    protected static final Logger logger = LoggerFactory.getLogger(MetricsManager.class);
 
     MetricRegistry metricsManager = MetricsManager.getMetricRegistry();
 
@@ -25,9 +32,19 @@ public class JVMHealthCheck extends HealthCheck {
         boolean healthy = true;
 
         // Validate if it is healthy
-        if ((Double) heapUsage.getValue() > maxHeapUsage || ((Double) nonHeapUsage.getValue() > maxHeapUsage)) {
-            healthy = false;
+        // if ((Double) heapUsage.getValue() > maxHeapUsage || ((Double) nonHeapUsage.getValue() > maxHeapUsage)) {
+        // healthy = false;
+        // }
+
+        Map<String, Metric> memoryMap = metricsManager.getMetrics();
+        for (String key : memoryMap.keySet()) {
+            Metric m = memoryMap.get(key);
+            logger.info(m.getClass().toString());
+            if (m instanceof Gauge) {
+                logger.info(((Gauge<Long>) m).getValue().toString());
+            }
         }
+
 
         if (healthy) {
             return Result.healthy("Heap Usage: %s");
--- src/test/java/emissary/core/JVMHealthCheckTest.java
+++ src/test/java/emissary/core/JVMHealthCheckTest.java
@@ -1,7 +1,5 @@
 package emissary.core;
 
-import static org.junit.Assert.assertTrue;
-
 import emissary.test.core.UnitTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +18,6 @@ public class JVMHealthCheckTest extends UnitTest {
         MetricsManager metricsManager = new MetricsManager();
         metricsManager.initMetrics();
         JVMHealthCheck healthCheck = new JVMHealthCheck(100, 100);
-        //assertTrue("Health check should pass", healthCheck.execute().isHealthy());
+        // assertTrue("Health check should pass", healthCheck.execute().isHealthy());
     }
 }
