333de7063980bf5a28a7f9f76c83ae9ced050b22
Connor <connor@connortech.me>
2025-05-15 06:08:27 -0400

    initial start of s3 pickup

---
+++ src/main/java/emissary/pickup/s3/S3DataServer.java
@@ -0,0 +1,141 @@
+package emissary.pickup.s3;
+
+import emissary.core.Pausable;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.rekognition.model.InvalidS3ObjectException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Thread to monitor a s3 bucket for files
+ */
+public class S3DataServer extends Pausable {
+
+    // Logger
+    protected static final Logger logger = LoggerFactory.getLogger(S3DataServer.class);
+
+    // How often to check for new messages in seconds
+    protected int pollingInterval = 10;
+    AmazonS3 s3;
+
+    protected String loadedBucket;
+    protected String inputBucket;
+
+    protected S3PickUpPlace myParent;
+
+    // Thread safe termination
+    protected boolean timeToShutdown = false;
+
+    public S3DataServer(S3PickUpPlace parent, int pollingInterval, AWSCredentials awsCredentials, String awsRegion, String inputBucket,
+            String loadedBucket) {
+        super("S3-" + inputBucket);
+        myParent = parent;
+        this.pollingInterval = pollingInterval;
+        this.inputBucket = inputBucket;
+        this.loadedBucket = loadedBucket;
+        this.s3 = amazonS3(awsCredentials, awsRegion);
+        this.setPriority(Thread.NORM_PRIORITY - 2);
+        this.setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        while (!timeToShutdown) {
+            if (checkPaused()) {
+                continue;
+            }
+
+            // Check the input for new files
+            ListObjectsV2Result result = s3.listObjectsV2(inputBucket);
+            List<S3ObjectSummary> objects = result.getObjectSummaries();
+            for (S3ObjectSummary objectSummary : objects) {
+                logger.info("Found object: {}", objectSummary.getKey());
+            }
+
+            int processedCount = 0;
+
+            for (S3ObjectSummary objectSummary : objects) {
+                logger.info("Processing object: {}", objectSummary.getKey());
+
+                S3Object object = s3.getObject(inputBucket, objectSummary.getKey());
+                byte[] objectPayload;
+                try {
+                    objectPayload = IOUtils.toByteArray(object.getObjectContent());
+                } catch (IOException e) {
+                    logger.error("Error reading S3 object", e);
+                    continue;
+                }
+
+                if (objectPayload == null) {
+                    logger.warn("Empty payload object");
+                    // TODO: Do something when empty payload
+                }
+
+                Boolean processed = myParent.processDataPayload(objectPayload, objectSummary.getKey());
+                if (processed) {
+                    // Move to loaded S3 Bucket
+                    moveObject(inputBucket, loadedBucket, objectSummary.getKey());
+                } else {
+                    // TODO: Move message to error bucket
+                }
+                processedCount++;
+            }
+
+            // Delay for the polling interval if there was
+            // nothing to do for this last round
+            if (processedCount == 0) {
+                try {
+                    Thread.sleep(pollingInterval * 1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+
+    }
+
+
+    private byte[] getS3Bytes(String bucket, String key) {
+        try {
+            S3Object object = s3.getObject(bucket, key);
+            return IOUtils.toByteArray(object.getObjectContent());
+        } catch (InvalidS3ObjectException | IOException e) {
+            logger.error("Error Fetching S3 Object", e);
+        }
+        return new byte[0];
+    }
+
+    private void moveObject(String sourceBucket, String targetBucket, String objectKey) {
+        // Copy to new bucket
+        s3.copyObject(sourceBucket, objectKey, targetBucket, objectKey);
+        // Delete it from old bucket
+        s3.deleteObject(sourceBucket, objectKey);
+    }
+
+    private AmazonS3 amazonS3(AWSCredentials credentials, String region) {
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+    /**
+     * Shutdown the thread
+     */
+    public void shutdown() {
+        timeToShutdown = true;
+    }
+
+}
---
+++ src/main/java/emissary/pickup/s3/S3PickUpPlace.java
@@ -0,0 +1,73 @@
+package emissary.pickup.s3;
+
+import emissary.core.DataObjectFactory;
+import emissary.core.EmissaryException;
+import emissary.core.IBaseDataObject;
+import emissary.pickup.IPickUpPlace;
+import emissary.pickup.PickUpPlace;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class S3PickUpPlace extends PickUpPlace implements IPickUpPlace {
+
+    private String awsAccessKey;
+    private String awsSecretKey;
+    private String awsRegion;
+    private AWSCredentials awsCredentials;
+    private String inputBucket;
+    private String loadedBucket;
+
+    // How often to check bucket in millis for new data
+    protected int pollingInterval = 30000;
+
+
+    public S3PickUpPlace() throws IOException {
+        super();
+        configurePlace();
+    }
+
+    protected void configurePlace() {
+        pollingInterval = configG.findIntEntry("POLLING_INTERVAL", pollingInterval);
+        awsAccessKey = configG.findStringEntry("AWS_ACCESS_KEY");
+        awsSecretKey = configG.findStringEntry("AWS_SECRET_KEY");
+        awsRegion = configG.findStringEntry("AWS_REGION");
+        inputBucket = configG.findStringEntry("S3_INPUT_BUCKET");
+        loadedBucket = configG.findStringEntry("s3_LOADED_BUCKET");
+        awsCredentials = credentials();
+        logger.info("S3 pickup configured for bucket: {} in region {}", inputBucket, awsRegion);
+        startDataServer();
+    }
+
+    public boolean processDataPayload(byte[] payload, String objectKey) {
+        IBaseDataObject dataObject = DataObjectFactory.getInstance(payload, objectKey);
+        dataObject.setParameter("originalFilename", objectKey);
+        dataObject.putParameter("processingDateTime", emissary.util.TimeUtil.getDateAsISO8601(new Date().getTime()));
+
+        dataObject.setCurrentForm("UNKNOWN"); // TODO: use config option
+        dataObject.setFileType("UNKNOWN");
+
+        try {
+            logger.info("**Deploying an agent for id: {}, object key: {}, forms: {}", dataObject.getInternalId(), objectKey,
+                    dataObject.getAllCurrentForms());
+            assignToPooledAgent(dataObject, -1L);
+        } catch (EmissaryException e) {
+            logger.error("Error deploying agent", e);
+            return false;
+        }
+        return true;
+    }
+
+    public AWSCredentials credentials() {
+        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    }
+
+
+    public void startDataServer() {
+        S3DataServer s3DataServer = new S3DataServer(this, pollingInterval, awsCredentials, awsRegion, inputBucket, loadedBucket);
+        s3DataServer.start();
+    }
+}
